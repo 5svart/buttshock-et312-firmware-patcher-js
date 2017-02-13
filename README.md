17f3638844f1a732d57225a5b8550d951ebd77c0
Kyle Machulis <kyle@nonpolynomial.com>
2017-02-13 09:58:00 -0800

    Clean out orphan'd gh-pages branch

--- README.md
+++
@@ -1,3 +0,0 @@
-# Buttshock ET312 Firmware Patcher
-
-Javascript utility for patching Erostek ET-312 firmware. Usable via node.js or the web.
--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  entry: './lib/buttshock-firmware-patcher-core.js',
-  output: {
-    path: './html',
-    filename: 'patcher.js',
-    libraryTarget: 'var',
-    library: 'bs'
-  }
-};
