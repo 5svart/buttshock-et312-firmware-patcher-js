48a6d6302a1b31bf40f482d4448c08c149b25095
Kyle Machulis <kyle@nonpolynomial.com>
2017-02-12 16:53:09 -0800

    test: Added patcher test. And it works on the first try? Scary.

--- test/tests.js
+++ test/tests.js
@@ -16,11 +16,12 @@ describe('ButtshockFirmwarePatcher Construction', function() {
   });
 });
 
-describe('ButtshockFirmwarePatcher Firmware Checks', function() {
+describe('ButtshockFirmwarePatcher Firmware Enc/Dec/Patching Checks', function() {
   // TODO: Fetch firmware!
   let fw15 = fs.readFileSync('firmware/312-15.upg');
   let fw16 = fs.readFileSync('firmware/312-16.upg');
   let FW16_MD5 = 'fc557679b91f4d59a95a83e0dbf3a4c8';
+  let M005_MD5 = '7333170d4cfbfe18059dcfd9d1ebf415';
   let bfp_enc = new bs.ButtshockFirmwarePatcher(fw16);
   let decrypted_fw = bfp_enc.decrypt();
   let bfp_dec = new bs.ButtshockFirmwarePatcher(decrypted_fw);
@@ -40,5 +41,13 @@ describe('ButtshockFirmwarePatcher Firmware Checks', function() {
     expect(bfp_dec).to.have.property('version', 'unknown');
     expect(encrypted_fw.length).to.equal(bs.ButtshockFirmwarePatcher.FIRMWARE_LENGTH);
     expect(md5(encrypted_fw)).to.equal(bs.ButtshockFirmwarePatcher.FIRMWARE_16_ENCRYPTED_MD5);
+  });
+  // TODO: test checksum
+  it('should have matching md5 for patching m005 over 312-16 firmware', function () {
+    let patches = fs.readFileSync('m005.fwpatch', 'ascii');
+    let patched_output;
+    expect(function () { patched_output = bfp_dec.applyPatches(patches); }).to.not.throw();
+    expect(patched_output.length).to.equal(bs.ButtshockFirmwarePatcher.FIRMWARE_LENGTH);
+    expect(md5(patched_output)).to.equal(M005_MD5);
   });
 });
